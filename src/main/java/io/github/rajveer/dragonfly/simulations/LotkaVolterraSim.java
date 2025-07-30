f0045a2ff979effb78f9882415b1763ba017c187
Rajveer Jadhav <rajveerjadhav08@gmail.com>
2025-07-30 20:07:49 +0530

    ODE Solvers and Systems built, with Vector Utils

---
+++ src/main/java/io/github/rajveer/dragonfly/simulations/LotkaVolterraSim.java
@@ -0,0 +1,33 @@
+package io.github.rajveer.dragonfly.simulations;
+
+import io.github.rajveer.dragonfly.ode.RK4Solver;
+import io.github.rajveer.dragonfly.systems.LotkaVolterraSystem;
+import io.github.rajveer.dragonfly.systems.ODESystem;
+import io.github.rajveer.dragonfly.utils.Vector;
+
+public class LotkaVolterraSim {
+
+    public static void main(String[] args){
+
+        ODESystem system = new LotkaVolterraSystem(
+                1.1,  // alpha: prey birth rate
+                0.4,  // beta: predation rate
+                0.1,  // delta: predator reproduction rate
+                0.4   // gamma: predator death rate
+        );
+
+        RK4Solver solver = new RK4Solver();  // or EulerSolver
+
+        Vector y = new Vector(new double[]{10.0, 5.0}); // [prey, predator]
+        double t = 0;
+        double dt = 0.1;
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("t = " + t + ", prey = " + y.get(0) + ", predator = " + y.get(1));
+            y = solver.step(system, t, y, dt);
+            t += dt;
+        }
+
+    }
+
+}
